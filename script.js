36506b3ae2538dcc4b2e66a27eedc1a09f8b5656
Younous Saleh <younisssaleh10@gmail.com>
2022-10-20 12:20:47 -0400

    Adding functionality to the eraser and setting background

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const context = canvas.getContext("2d");
 let currentSize = 10;
 let bucketColor = "#FFFFFF";
 let currentColor = "#A51DAB";
-// let isEraser = false;
+let isEraser = false;
 // let isMouseDown = false;
 // let drawnArray = [];
 
@@ -33,7 +33,45 @@ function createCanvas() {
   context.fillRect(0, 0, canvas.width, canvas.height); // x,y,width,height
 
   body.appendChild(canvas);
+  switchToBrush();
 }
+
+
+// Stettin brush color
+brushColorBtn.addEventListener("change", () => {
+  isEraser = false;
+  currentColor = `#${brushColorBtn.value}`;
+});
+
+// Setting up Eraser
+eraser.addEventListener("click", () => {
+  isEraser = true;
+
+  brushIcon.style.color = "white";
+  eraser.style.color = "black";
+  activeToolEl.textContent = "Eraser";
+  currentColor = bucketColor;
+  currentSize = 50;
+});
+
+// Switch back to Brush
+function switchToBrush() {
+  isEraser = false;
+  activeToolEl.textContent = "Brush";
+  brushIcon.style.color = "black";
+  eraser.style.color = "white";
+  currentColor = `#${brushColorBtn.value}`;
+  currentSize = 10;
+}
+
+// Setting background color
+bucketColorBtn.addEventListener("change", () => {
+  bucketColor = `#${bucketColorBtn.value}`; // setting our background color
+  createCanvas();
+});
+
+// Event Listener
+brushIcon.addEventListener("click", switchToBrush());
 
 // on load
 createCanvas();

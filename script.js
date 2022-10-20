4b519e974a57a538006638f543d04b89916b3a62
Younous Saleh <younisssaleh10@gmail.com>
2022-10-20 11:56:21 -0400

    Setup canvas

---
+++ script.js
@@ -0,0 +1,39 @@
+const activeToolEl = document.getElementById("active-tool");
+const brushColorBtn = document.getElementById("brush-color");
+const brushIcon = document.getElementById("brush");
+const brushSize = document.getElementById("brush-size");
+const brushSlider = document.getElementById("brush-slider");
+const bucketColorBtn = document.getElementById("bucket-color");
+const eraser = document.getElementById("eraser");
+const clearCanvasBtn = document.getElementById("clear-canvas");
+const saveStorageBtn = document.getElementById("save-storage");
+const loadStorageBtn = document.getElementById("load-storage");
+const clearStorageBtn = document.getElementById("clear-storage");
+const downloadBtn = document.getElementById("download");
+const { body } = document;
+
+// Global Variables
+// create our canvas
+const canvas = document.createElement("canvas");
+canvas.id = "canvas";
+const context = canvas.getContext("2d");
+
+let currentSize = 10;
+let bucketColor = "#FFFFFF";
+let currentColor = "#A51DAB";
+// let isEraser = false;
+// let isMouseDown = false;
+// let drawnArray = [];
+
+// create Canvas
+function createCanvas() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight - 50; // full height of the screen
+  context.fillStyle = bucketColor;
+  context.fillRect(0, 0, canvas.width, canvas.height); // x,y,width,height
+
+  body.appendChild(canvas);
+}
+
+// on load
+createCanvas();
